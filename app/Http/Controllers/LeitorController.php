38ff45533cba25ed1af6aa3a18e0f8e6dc2251e8
Luis Fernando <luis.bezerra2.estudante.ifms.edu.com>
2026-06-03 23:01:10 -0300

    controller/leitor/returning inertia refactored

--- app/Http/Controllers/LeitorController.php
+++ app/Http/Controllers/LeitorController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Leitor;
+use Inertia\Inertia;
 
 class LeitorController extends Controller
 {
-    public function cadastrarLeitor(){
-        return view("leitor/cadastrarLeitor");
+    public function cadastrarLeitor()
+    {
+        return Inertia::render('Leitor/CadastrarLeitor');
     }
 
-    public function salvarLeitor(Request $request){
-        
+    public function salvarLeitor(Request $request)
+    {
+
         $leitor = new Leitor();
         $leitor->nome = $request->nome;
         $leitor->documento = $request->documento;
@@ -22,8 +25,11 @@ class LeitorController extends Controller
         return redirect('cadastrarLeitor')->with('success', 'Leitor cadastrado com sucesso!');
     }
 
-    public function mostrarLeitor(){
+    public function mostrarLeitor()
+    {
         $leitores = Leitor::all();
-        return view('leitor/listarLeitor', compact('leitores'));
+        return Inertia::render('Leitor/ListarLeitor', [
+            'leitores' => $leitores
+        ]);
     }
 }
