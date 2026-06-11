da66b36405fbcb21242449df4896b0ba012315ae
Luis Fernando <luis.bezerra2.estudante.ifms.edu.com>
2026-06-10 21:21:13 -0300

    feat(historicoLeitor): configurar rotas, implementar historico de leitor, mostrar botao na listagem e definir tela do historico

--- resources/js/Pages/Leitor/ListarLeitor.vue
+++ resources/js/Pages/Leitor/ListarLeitor.vue
@@ -127,6 +127,12 @@ const leitoresFiltrados = computed(() => {
                             >
                                 Excluir
                             </button>
+
+                            <button @click="$inertia.visit('/historicoLeitor/' + leitor.id)"
+                                class="text-green-600 hover:underline ml-4"
+                            >
+                                Histórico
+                            </button>
                         </td>
                     </tr>
 
