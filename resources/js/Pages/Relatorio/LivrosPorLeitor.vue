12e3bdf6d545593de7b1969f6de89255b9f2d9f4
Luis Fernando <luis.bezerra2.estudante.ifms.edu.com>
2026-06-12 19:33:45 -0300

    feat(Relatorio): adding pagination into report page

--- resources/js/Pages/Relatorio/LivrosPorLeitor.vue
+++ resources/js/Pages/Relatorio/LivrosPorLeitor.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import AppLayout from '@/Layouts/AppLayout.vue'
+import {Link} from '@inertiajs/vue3'
 
 
 defineOptions({
@@ -8,91 +9,57 @@ defineOptions({
 })
 
 defineProps({
-    leitores: Array
+    leitores: Object
 })
 
 </script>
 
 <template>
-
-<div class="max-w-7xl mx-auto p-6">
-    
-    <!-- Cabeçalho -->
-    <div class="mb-8">
-
-        <h1 class="text-3xl font-bold text-gray-800">
-            Relatório de Livros por Leitor
-        </h1>
-
-    </div>
-
     <!-- Tabela -->
     <div class="bg-white rounded-xl border border-gray-200 shadow-sm overflow-hidden">
-
         <table class="w-full">
-
             <thead class="bg-gray-100">
-
                 <tr>
-                    <th class="p-4 text-left">
-                        Leitor
-                    </th>
-
-                    <th class="p-4 text-left">
-                        Quantidade
-                    </th>
-
-                    <th class="p-4 text-left">
-                        Livros
-                    </th>
+                    <th class="p-4 text-left">Leitor</th>
+                    <th class="p-4 text-left">Quantidade</th>
+                    <th class="p-4 text-left">Livros</th>
                 </tr>
-
             </thead>
-
             <tbody>
-
-                <tr
-                    v-for="leitor in leitores"
-                    :key="leitor.id"
-                    class="border-t align-top"
-                >
+                <tr v-for="leitor in leitores.data" :key="leitor.id" class="border-t align-top">
                     <td class="p-4 font-medium text-gray-800">
                         {{ leitor.nome }}
                     </td>
-
-                    <!-- Quantidade -->
                     <td class="p-4">
-                        <span
-                            class="bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-sm font-semibold"
-                        >
-                            {{ leitor.locacoes.length }}
+                        <span class="bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-sm font-semibold">
+                            {{ leitor.locacoes_count }}
                         </span>
                     </td>
-                    <!-- Lista livros -->
                     <td class="p-4">
-                        <div
-                            class="flex flex-wrap gap-2"
-                        >
-                            <span
-                                v-for="locacao in leitor.locacoes"
-                                :key="locacao.id"
-                                class="bg-gray-100 text-gray-700 px-3 py-1 rounded-lg text-sm"
-                            >
+                        <div class="flex flex-wrap gap-2">
+                            <span v-for="locacao in leitor.locacoes" :key="locacao.id" class="bg-gray-100 text-gray-700 px-3 py-1 rounded-lg text-sm">
                                 {{ locacao.livro.titulo }}
                             </span>
-
                         </div>
-
                     </td>
-
                 </tr>
-
             </tbody>
-
         </table>
-
     </div>
 
-</div>
-
+    <!-- Botões de Paginação com Tailwind -->
+    <div class="flex item-center justify-between mt-4">
+        <div class="flex gap-1">
+            <Link
+                v-for="(link, index) in leitores.links"
+                :key="index"
+                :href="link.url || '#'"
+                v-html="link.label"
+                class="px-3 py-2 border rounded"
+                :class="{
+                    'bg-blue-600 text-white border-blue-600': link.active
+                }"
+            />
+        </div>
+    </div>
 </template>
